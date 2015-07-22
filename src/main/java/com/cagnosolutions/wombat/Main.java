693f71323e1d48c8cb87421da025c7b9ec40a346
scottcagno <scottiecagno@gmail.com>
2015-07-22 12:20:35 -0400

    wombat-db[master] -- first commit

---
+++ src/main/java/com/cagnosolutions/wombat/Main.java
@@ -0,0 +1,12 @@
+package com.cagnosolutions.wombat;
+
+/**
+ * Created by Scott Cagno.
+ * Copyright Cagno Solutions. All rights reserved.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+	}
+}
